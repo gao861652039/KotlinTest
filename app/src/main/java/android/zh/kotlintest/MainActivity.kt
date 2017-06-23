534040146cb5bf8407703d72aee286c59a6fc0d4
Mr.feng <861652039@qq.com>
2017-06-23 14:43:41 +0800

    init KotlinTest repo

---
+++ app/src/main/java/android/zh/kotlintest/MainActivity.kt
@@ -0,0 +1,15 @@
+package android.zh.kotlintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main_tv_message.text = "Hello Kotlin!"
+        main_tv_message.textSize = 20.0f
+    }
+}
